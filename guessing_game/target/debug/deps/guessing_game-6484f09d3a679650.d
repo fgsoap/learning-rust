e0dd4a98c12953626a18218049d2e31a743e6d7f
Scott Xu <jianx2@cisco.com>
2020-09-24 10:04:01 +0800

    Revert "fix"
    This reverts commit 0674dadfd47f0d5521f50a3d6ee7dba1bfe49add.

--- guessing_game/target/debug/deps/guessing_game-6484f09d3a679650.d
+++
@@ -1,5 +0,0 @@
-/home/scottxu/rust/learning-rust/guessing_game/target/debug/deps/guessing_game-6484f09d3a679650: src/main.rs
-
-/home/scottxu/rust/learning-rust/guessing_game/target/debug/deps/guessing_game-6484f09d3a679650.d: src/main.rs
-
-src/main.rs:
--- guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/home/scottxu/rust/learning-rust/guessing_game/target/debug/guessing_game: /home/scottxu/rust/learning-rust/guessing_game/src/main.rs
