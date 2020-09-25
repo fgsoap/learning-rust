df0df9070600cd1bc1e4521ea8e12fc8c1b44459
Scott Xu <jianx2@cisco.com>
2020-09-25 11:38:25 +0800

    add workspace

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,8 +1,16 @@
+use std::cmp::Ordering;
+
 fn main() {
-    let x = 5;
+    let x = 6;
     println!("The value of x is: {}", x);
-    if x == 5 {
-        println!("Good!");
+    match 5.cmp(&x) {
+        Ordering::Equal => {
+            println!("Equal!");
+        }
+        Ordering::Less => {
+            println!("Less!");
+        }
+        _ => {}
     }
     println!("The value of x is: {}", x);
 }
