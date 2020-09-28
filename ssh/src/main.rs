046162b47aeb3b463abb90d4762fec704f308106
Scott Xu <jianx2@cisco.com>
2020-09-28 22:58:06 +0800

    add project ssh

---
+++ ssh/src/main.rs
@@ -0,0 +1,58 @@
+use std::io;
+use std::io::Read;
+use std::net::TcpStream;
+
+use ssh2::Session;
+
+fn main()
+{
+    let mut ssh_uri = String::new();
+
+    println!("Please input the Server IP and SSH Port: IP:Port");
+    io::stdin()
+        .read_line(&mut ssh_uri)
+        .expect("Failed to read SSH URI.");
+    let ssh_uri: String = match ssh_uri.trim().parse() {
+        Ok(str) => str,
+        Err(err) => err.to_string(),
+    };
+
+    let tcp = TcpStream::connect(&ssh_uri).unwrap();
+    let mut sess = Session::new().unwrap();
+    sess.set_tcp_stream(tcp);
+    sess.handshake().unwrap();
+
+    let mut username = String::new();
+    let mut password = String::new();
+
+    println!("Please input your username:");
+    io::stdin()
+        .read_line(&mut username)
+        .expect("Failed to read username.");
+
+    let username: String = match username.trim().parse() {
+        Ok(str) => str,
+        Err(err) => err.to_string(),
+    };
+
+    println!("Please input your password:");
+    io::stdin()
+        .read_line(&mut password)
+        .expect("Failed to read password.");
+
+    let password: String = match password.trim().parse() {
+        Ok(str) => str,
+        Err(err) => err.to_string(),
+    };
+
+    sess.userauth_password(&username, &password).unwrap();
+
+    assert!(sess.authenticated());
+
+    let mut channel = sess.channel_session().unwrap();
+    channel.exec("cat /var/log/*.log").unwrap();
+    let mut s = String::new();
+    channel.read_to_string(&mut s).unwrap();
+    println!("{}", s);
+    println!("{}", channel.exit_status().unwrap());
+}
