1ce938dfb44ddba46af7ca999672cfbce147cf2f
Scott Xu <jianx2@cisco.com>
2020-09-28 10:54:35 +0800

    add break

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,5 +1,11 @@
 fn main() {
-    loop {
-        println!("Hello, world!");
-    }
+    let mut counter = 0;
+    let result = loop {
+        println!("The value of counter is {}", counter);
+        counter += 1;
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+    println!("The result is {}", result);
 }
