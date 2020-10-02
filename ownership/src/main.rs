1c99230561ec6cdc749a12017f0d6e722822010e
Scott Xu <jianx2@cisco.com>
2020-10-02 10:13:13 +0800

    add more example

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -28,6 +28,26 @@ fn main() {
     makes_copy(x);
     // still keep x
     println!("{}", x);
+
+    // s1 get value from gives_ownership()
+    let s1 = gives_ownership();
+    println!("s1 is: {}", s1);
+
+    let s2 = String::from("hello");
+    println!("s2 is: {}", s2);
+
+    let s3 = takes_and_gives_back(s2);
+    println!("s3 is: {}", s3);
+}
+
+fn gives_ownership() -> String {
+    let some_string = String::from("hello");
+
+    some_string
+}
+
+fn takes_and_gives_back(a_string: String) -> String {
+    a_string
 }
 
 fn takes_ownership(some_string: String) {
