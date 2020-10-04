1ba7e401da104fbce357b37490225b36e7321156
Scott Xu <jianx2@cisco.com>
2020-10-04 11:35:02 +0800

    add fn

--- first_word/src/main.rs
+++ first_word/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let mut s = String::from("Hello World");
+
+    let word = first_word(&s); // get 5
+
+    s.clear(); // clear s
+
+    println!("{}", word); // word is useless as s is cleared
+}
+
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+    s.len()
 }
