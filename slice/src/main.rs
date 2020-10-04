57ae241f407912800a101c10b5cc72a2185fe6ff
Scott Xu <jianx2@cisco.com>
2020-10-04 11:57:44 +0800

    tried more on slices

--- slice/src/main.rs
+++ slice/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let s = String::from("Hello, world!");
+
+    let len = s.len();
+
+    let slice1 = &s[3..len];
+
+    let slice2 = &s[3..];
+
+    let slice3 = &s[0..2];
+
+    let slice4 = &s[..];
+
+    println!("{}, {}, {}, {}", slice1, slice2, slice3, slice4);
 }
