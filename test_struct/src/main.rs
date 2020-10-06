80faa6a0af635da42ed70debf9f961943df8b7c4
Scott Xu <jianx2@cisco.com>
2020-10-06 17:09:56 +0800

    add more examples to test_struct

--- test_struct/src/main.rs
+++ test_struct/src/main.rs
@@ -7,11 +7,25 @@ fn main() {
         active: bool,
     }
 
-    let user = User {
+    let mut user1 = User {
         email: String::from("someone@example.com"),
         username: String::from("someusername123"),
         active: true,
         sign_in_count: 1,
     };
-    println!("{:?}", user);
+    user1.email = String::from("anotheremail@example.com");
+    println!("{:#?}", user1);
+
+    fn build_user(email: String, username: String) -> User {
+        User {
+            email,
+            username,
+            active: true,
+            sign_in_count: 1,
+        }
+    }
+    println!(
+        "{:#?}",
+        build_user(String::from("test@example.com"), String::from("test"))
+    );
 }
