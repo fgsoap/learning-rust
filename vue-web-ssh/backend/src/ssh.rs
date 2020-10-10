03631cf108525b17ee06205d67e9ee9f52608e2c
Scott Xu <jianx2@cisco.com>
2020-10-10 14:12:43 +0800

    add vue-web-ssh

---
+++ vue-web-ssh/backend/src/ssh.rs
@@ -0,0 +1,48 @@
+use actix_web::web;
+use serde::{Deserialize, Serialize};
+use std::io::Read;
+use std::net::TcpStream;
+
+use log::{info, warn};
+use ssh2::Session;
+
+pub async fn exec(info: web::Json<Info>) -> web::Json<Result> {
+    info!("Start to connect to {}.", info.ssh_uri.clone());
+    let tcp = TcpStream::connect(info.ssh_uri.clone()).unwrap();
+    let mut sess = Session::new().unwrap();
+    sess.set_tcp_stream(tcp);
+    sess.handshake().unwrap();
+    let mut auth_err = String::new();
+    sess.userauth_password(&info.username.clone(), &info.password.clone())
+        .unwrap_or_else(|error| {
+            auth_err = error.to_string();
+        });
+    if !sess.authenticated() {
+        warn!("{}", auth_err);
+        return web::Json(Result {
+            result: auth_err.to_string(),
+        });
+    }
+    info!("Authenticated!");
+    // assert!(sess.authenticated());
+    let mut channel = sess.channel_session().unwrap();
+    channel.exec(&info.command.clone()).unwrap();
+    let mut s = String::new();
+    channel.read_to_string(&mut s).unwrap();
+    channel.close().unwrap();
+    info!("Disconnect from {} successfully.", info.ssh_uri.clone());
+    web::Json(Result { result: s })
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Info {
+    ssh_uri: String,
+    username: String,
+    password: String,
+    command: String,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Result {
+    result: String,
+}
---
+++ vue-web-ssh/frontend/src/app.vue
@@ -0,0 +1,193 @@
+<template>
+  <div id="app">
+    <div style="text-align: center">
+      <div id="title">
+        <a to="#">Welcome to Web SSH</a>
+      </div>
+      <br />
+
+      <label>Server: </label>
+      <select style="width: 10%" v-model="SSH" required>
+        <option value="10.252.218.71:22" selected="selected">
+          10.252.218.71
+        </option>
+        <option value="10.252.218.73:22">10.252.218.73</option>
+      </select>
+      <br />
+
+      <!-- <input
+        style="width: 10%"
+        type="text"
+        name="ssh"
+        placeholder="SSH"
+        v-model="SSH"
+        required
+      /> -->
+      <input
+        style="width: 10%"
+        type="text"
+        name="username"
+        placeholder="Username"
+        v-model="Username"
+        required
+      />
+      <input
+        style="width: 10%"
+        type="password"
+        name="password"
+        placeholder="Password"
+        v-model="Password"
+        required
+      />
+      <br />
+
+      <div style="display: flex">
+        <input
+          style="width: 50%"
+          type="text"
+          name="command"
+          placeholder="Please input your command here!"
+          v-model="Command"
+          required
+        />
+      </div>
+      <br />
+
+      <button id="submit" @click="exec">Execute</button>
+      <br />
+
+      <h2 class="content-subhead">Showing the Result!</h2>
+    </div>
+    <div class="content-wrapper">
+      <div class="" style="display: flex">
+        <div class="code">
+          <pre
+            style="display: block; white-space: pre; margin: 1em 0"
+          ><code>{{ Result }}</code>
+        </pre>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      Result: "",
+    };
+  },
+  methods: {
+    exec() {
+      let ssh_uri = this.SSH;
+      let username = this.Username;
+      let password = this.Password;
+      let command = this.Command;
+
+      let data = {
+        ssh_uri: ssh_uri,
+        username: username,
+        password: password,
+        command: command,
+      };
+      fetch("http://localhost:8000/ssh", {
+        body: JSON.stringify(data),
+        headers: {
+          "content-type": "application/json",
+        },
+        method: "POST",
+      })
+        .then((response) => response.json())
+        .then((json) => {
+          console.log(json);
+          this.Result = json.result;
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+  },
+};
+</script>
+
+<style scoped>
+#content {
+  width: 250px;
+  margin: 0 auto;
+  padding-top: 33px;
+}
+#title {
+  padding: 0.5rem 0;
+  font-size: 22px;
+  font-weight: bold;
+  background-color: bisque;
+  text-align: center;
+}
+input[type="text"],
+input[type="password"] {
+  margin: 6px auto auto;
+  width: 250px;
+  height: 36px;
+  border: none;
+  border-bottom: 1px solid #aaa;
+  font-size: 16px;
+}
+#submit {
+  margin: 10px 0 20px 0;
+  width: 250px;
+  height: 33px;
+  background-color: bisque;
+  border: none;
+  border-radius: 2px;
+  font-family: "Roboto", sans-serif;
+  font-weight: bold;
+  transition: 0.1s ease;
+  cursor: pointer;
+}
+input[type="checkbox"] {
+  margin-top: 11px;
+}
+dialog {
+  top: 50%;
+  width: 80%;
+  border: 5px solid rgba(0, 0, 0, 0.3);
+}
+dialog::backdrop {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.7);
+}
+#closeDialog {
+  display: inline-block;
+  border-radius: 3px;
+  border: none;
+  font-size: 1rem;
+  padding: 0.4rem 0.8em;
+  background: #eb9816;
+  border-bottom: 1px solid #f1b75c;
+  color: white;
+  font-weight: bold;
+  text-align: center;
+}
+#closeDialog:hover,
+#closeDialog:focus {
+  opacity: 0.92;
+  cursor: pointer;
+}
+#user-info {
+  width: 250px;
+  margin: 0 auto;
+  padding-top: 44px;
+}
+@media only screen and (min-width: 600px) {
+  #content {
+    margin: 0 auto;
+    padding-top: 100px;
+  }
+}
+</style>
