634dff64bbd390c60020fcedfa772a66c131f8bf
Scott Xu <jianx2@cisco.com>
2020-10-15 12:33:05 +0800

    add match

--- test_enum/src/main.rs
+++ test_enum/src/main.rs
@@ -12,6 +12,28 @@ enum Message<'t> {
     ChangeColor(i32, i32, i32),
 }
 
+impl Message<'_> {
+    fn call(&self) {
+        println!("{:?}", &self)
+    }
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
 fn main() {
     let four = IpAddrKind::V4(127, 0, 0, 1);
     let six = IpAddrKind::V6(String::from("::1"));
@@ -29,15 +51,14 @@ fn main() {
     let c = Message::ChangeColor(23, 34, 56);
     c.call();
 
+    // test Option
     let some_number = Some(5);
     let some_string = Some("a string");
 
     let absent_number: Option<i32> = None;
     println!("{:?}, {:?}, {:?}", some_number, some_string, absent_number);
-}
 
-impl Message<'_> {
-    fn call(&self) {
-        println!("{:?}", &self)
-    }
+    // test match
+    let my_coin = Coin::Penny;
+    println!("{}", value_in_cents(my_coin));
 }
