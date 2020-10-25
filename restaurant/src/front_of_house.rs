0508ee04e6b366a602226571456b0685365e5c3f
Scott Xu <jianx2@cisco.com>
2020-10-25 09:20:38 +0000

    refactor: split mod to different files

---
+++ restaurant/src/front_of_house.rs
@@ -0,0 +1,17 @@
+pub mod hosting {
+    pub fn add_to_waitlist() {}
+
+    #[allow(dead_code)]
+    fn seat_at_table() {}
+}
+
+mod serving {
+    #[allow(dead_code)]
+    fn take_order() {}
+
+    #[allow(dead_code)]
+    fn serve_order() {}
+
+    #[allow(dead_code)]
+    fn take_payment() {}
+}
--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,24 +1,6 @@
-use crate::front_of_house::hosting;
+mod front_of_house
 
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-
-        #[allow(dead_code)]
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        #[allow(dead_code)]
-        fn take_order() {}
-
-        #[allow(dead_code)]
-        fn serve_order() {}
-
-        #[allow(dead_code)]
-        fn take_payment() {}
-    }
-}
+pub use crate::front_of_house::hosting;
 
 pub fn eat_at_restaurant() {
     hosting::add_to_waitlist();
