35d386c447e9c80159d1ba230189e1a9e2c33f84
Scott Xu <jianx2@cisco.com>
2020-10-27 08:18:02 +0000

    style: format test_vec

--- test_vec/src/main.rs
+++ test_vec/src/main.rs
@@ -9,12 +9,12 @@ fn main() {
     println!("Hello, world! {:?}", v);
 
     {
-        let v = vec![1,2,3,4,5];
+        let v = vec![1, 2, 3, 4, 5];
         let third: &i32 = &v[2];
         println!("The third element is {}", third);
         match v.get(2) {
-            Some(third)=>println!("The third element is {}", third),
-            None=>println!("There is no third element."),
+            Some(third) => println!("The third element is {}", third),
+            None => println!("There is no third element."),
         }
         // let does_not_exist = &v[100];
         let does_not_exist = v.get(100);
@@ -22,7 +22,7 @@ fn main() {
     }
 
     {
-        let mut v = vec![1,2,3,4,5];
+        let mut v = vec![1, 2, 3, 4, 5];
         v.push(6);
         let first = &v[5];
         // if try to add 6 to vec v, the physical address may be changed, so the below method of v will fail
@@ -32,10 +32,9 @@ fn main() {
 
     // loop vec
     {
-        let v = vec![100,32,57];
+        let v = vec![100, 32, 57];
         for i in &v {
-            println!("{}",i);
+            println!("{}", i);
         }
     }
-    
 }
