820a33759ab05743dd66619ba77389eaf0e9f18a
Scott Xu <jianx2@cisco.com>
2020-10-27 08:35:23 +0000

    feat: add emum to vec

--- test_vec/src/main.rs
+++ test_vec/src/main.rs
@@ -44,4 +44,21 @@ fn main() {
             println!("{}", i);
         }
     }
+
+    // add emum to vec
+    #[derive(Debug)]
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
+
+    for i in &row {
+        println!("{:?}", i);
+    }
 }
