33243bbc3df76ce2d00d8a59611f0e8085f40d62
Scott Xu <jianx2@cisco.com>
2020-10-27 08:25:57 +0000

    feat: add loop mut vec

--- test_vec/src/main.rs
+++ test_vec/src/main.rs
@@ -37,4 +37,11 @@ fn main() {
             println!("{}", i);
         }
     }
+    {
+        let mut v = vec![100, 32, 57];
+        for i in &mut v {
+            *i += 50;
+            println!("{}", i);
+        }
+    }
 }
