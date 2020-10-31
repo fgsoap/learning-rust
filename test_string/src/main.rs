c33c5f3e1b6802069965869e079b701a4fc88fc4
Scott Xu <fgsoap@gmail.com>
2020-10-31 16:36:03 +0800

    test: add char

--- test_string/src/main.rs
+++ test_string/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    // let mut s = String::new();
+    let data = "initial contents";
+    let s = data.to_string();
+    println!("data to string: {}", s);
+    let s = "initial contents".to_string();
+    println!("literal to string: {}", s);
+    // test char
+    let s = 'h';
+    println!("charactor is: {}", s);
 }
