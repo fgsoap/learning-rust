81b1c5481b72590686a303e3f9a239480105c916
Scott Xu <jianx2@cisco.com>
2020-11-01 09:15:21 +0000

    test: try

--- test_string/src/main.rs
+++ test_string/src/main.rs
@@ -19,8 +19,20 @@ fn main() {
     println!("after push: {}", s);
 
     let mut s1 = String::from("foo");
-    let s2= "bar";
+    let s2 = "bar";
     s1.push_str(&s2); // s1.push_str(s2)
     println!("s2 is: {}", s2);
 
+    // test `+` & `format!`
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    let s3 = s1 + &s2;
+    println!("s3 is: {}", s3);
+    println!("{}", s2);
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+    let s = format!("{}-{}-{}", s1, s2, s3);
+    println!("after s is: {}", s);
 }
