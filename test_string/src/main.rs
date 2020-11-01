04a311c7cc07ac7ac10baa36a5a0a70893eb39ce
Scott Xu <jianx2@cisco.com>
2020-11-01 08:26:22 +0000

    test: push & push_str

--- test_string/src/main.rs
+++ test_string/src/main.rs
@@ -8,4 +8,19 @@ fn main() {
     // test char
     let s = 'h';
     println!("charactor is: {}", s);
+    let s = String::from("initial contents");
+    println!("String::from dada is: {}", s);
+
+    // test push
+    let mut s = String::from("foo");
+    println!("before push: {}", s);
+    s.push_str("bar");
+    s.push('s');
+    println!("after push: {}", s);
+
+    let mut s1 = String::from("foo");
+    let s2= "bar";
+    s1.push_str(&s2); // s1.push_str(s2)
+    println!("s2 is: {}", s2);
+
 }
