647530e562ecca8ae1ca2caf87fedfad64b4081b
Scott Xu <jianx2@cisco.com>
2020-11-04 14:13:00 +0000

    feat: try collect to make a hashmap

--- test_hashmap/src/main.rs
+++ test_hashmap/src/main.rs
@@ -6,4 +6,11 @@ fn main() {
     scores.insert(String::from("Yellow"), 50);
 
     println!("{:?}", scores);
+
+    // test collect to make a HashMap
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
+
+    let scores: HashMap<_,_> = teams.iter().zip(initial_scores.iter()).collect();
+    println!("{:?}", scores);
 }
