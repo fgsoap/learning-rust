3f2d5d9ed45e8380a2dbc0245cdc4bcde603bd46
Scott Xu <fgsoap@gmail.com>
2020-11-05 23:11:47 +0800

    feat: test ownership in HashMap

--- test_hashmap/src/main.rs
+++ test_hashmap/src/main.rs
@@ -13,4 +13,18 @@ fn main() {
 
     let scores: HashMap<_,_> = teams.iter().zip(initial_scores.iter()).collect();
     println!("{:?}", scores);
+
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+    // from here, try to use field_name&field_value will have a compile failure
+    let another_field_name = 34;
+    let another_field_value = String::from("Pink");
+    let mut another_map = HashMap::new();
+    another_map.insert(another_field_name, another_field_value);
+    // another_field_name is fine as it implements Copy trait
+    println!("{:?}", another_field_name);
+
 }
