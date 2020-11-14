6b5efe389c556889bbe22137002438955aaf3f34
Scott Xu <jianx2@cisco.com>
2020-11-14 03:51:38 +0000

    test: test

--- test_panic/src/main.rs
+++ test_panic/src/main.rs
@@ -1,4 +1,5 @@
 use std::fs::File;
+use std::io::ErrorKind;
 
 fn main() {
     // let v = vec![1, 2, 3];
@@ -9,6 +10,13 @@ fn main() {
 
     let f = match f {
         Ok(file) => file,
-        Err(error) => panic!("There wa s problem opening the file: {:?}", error),
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Tried to create file but there was a problem: {:?}", e),
+            },
+            other_error => panic!("There wa s problem opening the file: {:?}", other_error),
+        },
     };
+    println!("{:?}", f);
 }
