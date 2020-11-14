372f274f0342ff304f9e6c45487a43bbbdcf0189
Scott Xu <jianx2@cisco.com>
2020-11-14 03:15:49 +0000

    test: test

--- test_panic/src/main.rs
+++ test_panic/src/main.rs
@@ -1,5 +1,14 @@
+use std::fs::File;
+
 fn main() {
-    let v = vec![1, 2, 3];
-    v[99];
-    println!("Hello, world!");
+    // let v = vec![1, 2, 3];
+    // v[99];
+    // println!("Hello, world!");
+
+    let f = File::open("hello.txt");
+
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => panic!("There wa s problem opening the file: {:?}", error),
+    };
 }
