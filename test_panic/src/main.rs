98209e1dc4e9b9ae8f37490b52edcb31f6ba1fe9
Scott Xu <jianx2@cisco.com>
2020-11-15 01:34:42 +0000

    test: test unwrap

--- test_panic/src/main.rs
+++ test_panic/src/main.rs
@@ -6,17 +6,17 @@ fn main() {
     // v[99];
     // println!("Hello, world!");
 
-    let f = File::open("hello.txt");
+    let f = File::open("hello.txt").unwrap();
 
-    let f = match f {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create("hello.txt") {
-                Ok(fc) => fc,
-                Err(e) => panic!("Tried to create file but there was a problem: {:?}", e),
-            },
-            other_error => panic!("There wa s problem opening the file: {:?}", other_error),
-        },
-    };
-    println!("{:?}", f);
+    // let f = match f {
+    //     Ok(file) => file,
+    //     Err(error) => match error.kind() {
+    //         ErrorKind::NotFound => match File::create("hello.txt") {
+    //             Ok(fc) => fc,
+    //             Err(e) => panic!("Tried to create file but there was a problem: {:?}", e),
+    //         },
+    //         other_error => panic!("There wa s problem opening the file: {:?}", other_error),
+    //     },
+    // };
+    // println!("{:?}", f);
 }
