1ce16c3530b02f36d95a901e879636433f21032a
Scott Xu <jianx2@cisco.com>
2020-11-15 03:59:02 +0000

    feat: test more

--- test_panic/src/main.rs
+++ test_panic/src/main.rs
@@ -1,5 +1,24 @@
 use std::fs::File;
-use std::io::ErrorKind;
+use std::io;
+// use std::io::ErrorKind;
+use std::io::Read;
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => {
+            // println!("{:?}", e);
+            return Err(e);
+        }
+    };
+
+    let mut s = String::new();
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
 
 fn main() {
     // let v = vec![1, 2, 3];
@@ -7,7 +26,7 @@ fn main() {
     // println!("Hello, world!");
 
     // let f = File::open("hello.txt").unwrap();
-    let f = File::open("hello.txt").expect("Failed to open hello.txt");
+    // let f = File::open("hello.txt").expect("Failed to open hello.txt");
 
     // let f = match f {
     //     Ok(file) => file,
@@ -19,5 +38,7 @@ fn main() {
     //         other_error => panic!("There wa s problem opening the file: {:?}", other_error),
     //     },
     // };
-    // println!("{:?}", f);
+
+    let f = read_username_from_file();
+    println!("{:?}", f);
 }
