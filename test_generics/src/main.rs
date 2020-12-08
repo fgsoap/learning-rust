82b9fa57a6a46f84e49c99ae1b4213b783f67a58
fgsoap <fgsoap@gmail.com>
2020-12-08 20:22:38 +0800

    feat: add fn to test_generics

--- test_generics/src/main.rs
+++ test_generics/src/main.rs
@@ -1,10 +1,22 @@
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
-    let mut largest = number_list[0];
-    for number in number_list {
-        if number > largest {
-            largest = number
+    // let mut largest = number_list[0];
+    // for number in number_list {
+    //     if number > largest {
+    //         largest = number
+    //     }
+    // }
+    let result = largest(&number_list);
+    println!("The laegest number is {}", result)
+}
+
+fn largest(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
         }
     }
-    println!("The laegest number is {}", largest)
+    largest
 }
