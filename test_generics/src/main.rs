eff75e0743c5983b3c37945b9ee800c703c0cbf8
fgsoap <fgsoap@gmail.com>
2021-01-01 20:46:12 +0800

    feat: add generic implementation

--- test_generics/src/main.rs
+++ test_generics/src/main.rs
@@ -1,25 +1,20 @@
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
-    let result = largest(&number_list);
-    println!("The laegest number is {}", result);
-
-    let both_integer = Point { x: 5, y: 10 };
-    let both_float = Point { x: 1.0, y: 4.0 };
-    let integer_and_float = Point { x: 5, y: 4.0 };
-}
-
-fn largest<T>(list: &[T]) -> T {
-    let mut largest = list[0];
-
-    for &item in list.iter() {
-        if item > largest {
-            largest = item;
-        }
-    }
-    largest
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: "c" };
+    let p3 = p1.mixup(p2);
+    println!("p3.x={},p3.y={}", p3.x, p3.y);
 }
 
 struct Point<T, U> {
     x: T,
     y: U,
+}
+
+impl<T, U> Point<T, U> {
+    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
 }
