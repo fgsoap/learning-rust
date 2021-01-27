39e27eda684e63b9d79c3f0e35613b35b7d6b253
fgsoap <fgsoap@gmail.com>
2021-01-27 14:48:57 +0800

    add default trait impl

--- test_trait/src/lib.rs
+++ test_trait/src/lib.rs
@@ -1,5 +1,7 @@
 pub trait Summary {
-    fn summarize(&self) -> String;
+    fn summarize(&self) -> String {
+        String::from("(Read more...)")
+    }
 }
 
 pub struct NewsArticle {
@@ -23,7 +25,7 @@ pub struct Tweet {
 }
 
 impl Summary for Tweet {
-    fn summarize(&self) -> String {
-        format!("{}: {}", self.username, self.content)
-    }
+    // fn summarize(&self) -> String {
+    //     format!("{}: {}", self.username, self.content)
+    // }
 }
