6e711dc42ac9ce1e45a6c2150d41b1acd1abf8de
fgsoap <fgsoap@gmail.com>
2021-01-27 12:15:38 +0800

    impl trait

--- test_trait/src/main.rs
+++ test_trait/src/main.rs
@@ -1,3 +1,11 @@
+use test_trait::Tweet;
+
 fn main() {
-    println!("Hello, world!");
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course,  as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+    println!("1 new tweet: {}", tweet.summarized());
 }
