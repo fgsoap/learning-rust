d4626406f6426f7b19e2269ce754990a9130469e
fgsoap <fgsoap@gmail.com>
2021-03-05 06:19:25 +0800

    changes for longest_string

--- longest_string/src/main.rs
+++ longest_string/src/main.rs
@@ -1,8 +1,10 @@
 fn main() {
     let s1 = String::from("Hello");
-    let s2 = "World";
-    let result = longest(s1.as_str(), s2);
-    println!("{}", result);
+    {
+        let s2 = "World";
+        let result = longest(s1.as_str(), s2);
+        println!("{}", result);
+    }
 }
 
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
