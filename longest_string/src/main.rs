cb14e6d93a03bb6794f957cf2d06ce6a6eefcf36
fgsoap <fgsoap@gmail.com>
2021-03-21 20:10:11 +0800

    add test adder

--- longest_string/src/main.rs
+++ longest_string/src/main.rs
@@ -1,3 +1,4 @@
+use std::fmt::Display;
 fn main() {
     let s1 = String::from("Hello");
     let result;
@@ -9,6 +10,19 @@ fn main() {
 }
 
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+#[warn(dead_code)]
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement!{}", ann);
     if x.len() > y.len() {
         x
     } else {
