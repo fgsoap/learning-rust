0c6a21b4cb2d1743c549df1d50a494d2b3ad72cd
fgsoap <fgsoap@gmail.com>
2021-04-05 09:09:54 +0800

    add should_panic

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -13,6 +13,27 @@ pub fn add_two(a: i32) -> i32 {
 }
 pub fn greeting(name: &str) -> String {
     format!("Hello {}!", name)
+    // String::from("Hello!")
+}
+#[allow(dead_code)]
+pub struct Guess {
+    value: u32,
+}
+impl Guess {
+    pub fn new(value: u32) -> Guess {
+        if value < 1 {
+            panic!(
+                "Guess value must be greater than or equal to 1, got {}.",
+                value
+            );
+        } else if value > 100 {
+            panic!(
+                "Guess value must be less than or equal to 100, got {}.",
+                value
+            );
+        }
+        Guess { value }
+    }
 }
 #[cfg(test)]
 mod tests {
@@ -20,13 +41,18 @@ mod tests {
     #[test]
     fn greeting_contains_name() {
         let result = greeting("Carol");
-        assert!(result.contains("Carol"));
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was `{}`",
+            result
+        );
     }
     #[test]
     fn it_adds_two() {
         assert_eq!(4, add_two(2));
     }
     #[test]
+    #[ignore = "reason"]
     fn exploration() {
         assert_eq!(2 + 2, 4);
     }
@@ -45,5 +71,23 @@ mod tests {
             width: 1,
         };
         assert!(larger.can_hold(&smaller))
+    }
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            length: 8,
+            width: 7,
+        };
+        let smaller = Rectangle {
+            length: 5,
+            width: 1,
+        };
+
+        assert!(!smaller.can_hold(&larger))
+    }
+    #[test]
+    #[should_panic(expected = "Guess value must be less than or equal to 100")]
+    fn greater_than_100() {
+        Guess::new(200);
     }
 }
