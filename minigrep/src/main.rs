994beef9c7428e19cfa43623988734e9213b3060
fgsoap <fgsoap@gmail.com>
2021-04-22 21:25:51 +0800

    add run

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,18 +1,24 @@
 use std::env;
+use std::error::Error;
 use std::fs;
 use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args).unwrap_or_else(|err|{
+    let config = Config::new(&args).unwrap_or_else(|err| {
         println!("Problem parsing argument: {}", err);
         process::exit(1);
     });
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
-
-    let contents =
-        fs::read_to_string(config.filename).expect("Something went wrong reading the file");
+    if let Err(e) = run(config) {
+        println!("Application error:{}", e);
+        process::exit(1);
+    }
+}
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
     println!("With text:\n{}", contents);
+    Ok(())
 }
 struct Config {
     query: String,
