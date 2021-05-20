3a02463a603a7311a5dc03a40bbdf45e92beb09e
fgsoap <fgsoap@gmail.com>
2021-05-20 17:27:31 +0800

    initial test_iter

--- test_iter/src/lib.rs
+++ test_iter/src/lib.rs
@@ -2,12 +2,15 @@
 fn iterator_demonstration() {
     let v1 = vec![1, 2, 3];
 
-    // let mut v1_iter = v1.iter();
+    let mut v1_iter = v1.iter();
+    assert_eq!(v1_iter.next(), Some(&1));
+    assert_eq!(v1_iter.next(), Some(&2));
+    assert_eq!(v1_iter.next(), Some(&3));
+
     let mut v2_iter = v1.into_iter();
-    // assert_eq!(v1_iter.next(), Some(&1));
-    // assert_eq!(v1_iter.next(), Some(&2));
-    // assert_eq!(v1_iter.next(), Some(&3));
     assert_eq!(v2_iter.next(), Some(1));
     assert_eq!(v2_iter.next(), Some(2));
     assert_eq!(v2_iter.next(), Some(3));
+
+    // let mut v3_iter = v1.iter_mut();
 }
