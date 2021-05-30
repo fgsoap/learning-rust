3dca60f69fc79de48b14384cf2d10c4247747fa5
fgsoap <fgsoap@gmail.com>
2021-05-30 11:00:03 +0800

    add collect

--- test_iter/src/lib.rs
+++ test_iter/src/lib.rs
@@ -14,3 +14,15 @@ fn iterator_demonstration() {
 
     // let mut v3_iter = v1.iter_mut();
 }
+
+#[test]
+fn iterator_sum() {
+    let v1 = vec![1, 2, 3];
+    let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
+    assert_eq!(v2, vec![2,3,4]);
+
+    let v1_iter = v1.iter();
+    let total: i32 = v1_iter.sum();
+
+    assert_eq!(total, 6);
+}
