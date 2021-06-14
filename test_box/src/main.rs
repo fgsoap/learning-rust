2dff00d2af26cea26311ab50fa6a567a44a3e64d
fgsoap <fgsoap@gmail.com>
2021-06-14 09:40:45 +0800

    add MyBox

--- test_box/src/main.rs
+++ test_box/src/main.rs
@@ -1,10 +1,17 @@
 use crate::List::{Cons, Nil};
+use std::ops::Deref;
 fn main() {
     let b = Box::new(5);
     println!("b = {}", b);
 
     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
     println!("list = {:?}", list);
+
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
 }
 
 #[derive(Debug)]
@@ -13,9 +20,25 @@ enum List {
     Nil,
 }
 
-enum Message {
-    Quit,
-    Move { x: i32, y: i32 },
-    Write(String),
-    ChangeColor(i32, i32, i32),
+// enum Message {
+//     Quit,
+//     Move { x: i32, y: i32 },
+//     Write(String),
+//     ChangeColor(i32, i32, i32),
+// }
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.0
+    }
 }
