3aa8aed784a05560f53206ae62cfe2ad93664fa2
fgsoap <fgsoap@gmail.com>
2021-09-26 06:03:02 +0800

    add front_of_house.rs

--- front_of_house/src/lib.rs
+++ front_of_house/src/lib.rs
@@ -12,14 +12,10 @@ mod tests {
     }
 }
 
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-    }
-}
+mod front_of_house;
 
-// use crate::front_of_house::hosting;
-use self::front_of_house::hosting;
+pub use crate::front_of_house::hosting;
+// use self::front_of_house::hosting;
 
 pub fn eat_at_restaurant() {
     hosting::add_to_waitlist();
