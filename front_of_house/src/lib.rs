45cd0629f34f3278cf81788b5f412d599e0be450
fgsoap <fgsoap@gmail.com>
2021-09-25 16:27:15 +0800

    add use

--- front_of_house/src/lib.rs
+++ front_of_house/src/lib.rs
@@ -1,7 +1,28 @@
 #[cfg(test)]
 mod tests {
+    use crate::eat_at_restaurant;
+
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
     }
+    #[test]
+    fn eat_at_restaurant_test() {
+        eat_at_restaurant();
+    }
+}
+
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+    }
+}
+
+// use crate::front_of_house::hosting;
+use self::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
 }
