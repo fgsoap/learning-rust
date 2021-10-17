359f100adf6304ef9538647ce0e4bc27238b0f2d
fgsoap <fgsoap@gmail.com>
2021-10-17 08:09:02 +0800

    test

--- test_string/src/main.rs
+++ test_string/src/main.rs
@@ -20,7 +20,8 @@ fn main() {
 
     let mut s1 = String::from("foo");
     let s2 = "bar";
-    s1.push_str(&s2); // s1.push_str(s2)
+    // s1.push_str(&s2);
+    s1.push_str(s2);
     println!("s2 is: {}", s2);
 
     // test `+` & `format!`
