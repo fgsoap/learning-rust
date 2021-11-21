cb9865d56732d8649054ac2a275f7546764a86c7
fgsoap <fgsoap@gmail.com>
2021-11-21 20:28:31 +0800

    add some stuff

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,3 +1,35 @@
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+    // let v = "hello world!";
+    // assert_eq!(v, "hello world!");
+    // let v = "hello Rust!";
+    // assert_eq!(v, "hello Rust!");
+    // {
+    //     let v = "hello World!";
+    //     assert_eq!(v, "hello World!");
+    // }
+    // assert_eq!(v, "hello Rust!");
+
+    let a = 2;
+    let b = 3;
+    assert_eq!(math(sum, a, b), 5);
+    assert_eq!(math(product, a, b), 6);
+    assert_eq!(true_maker()(), true);
+}
+
+pub fn math(op: fn(i32, i32) -> i32, a: i32, b: i32) -> i32 {
+    op(a, b)
+}
+
+fn sum(a: i32, b: i32) -> i32 {
+    a + b
+}
+fn product(a: i32, b: i32) -> i32 {
+    a * b
+}
+fn is_true() -> bool {
+    true
+}
+fn true_maker() -> fn() -> bool {
+    is_true
 }
