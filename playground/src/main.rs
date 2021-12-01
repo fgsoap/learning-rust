0c23d5d84752b2e25d6de9197b44b739c2e331c6
fgsoap <fgsoap@gmail.com>
2021-12-01 21:00:09 +0800

    add while let

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -62,12 +62,17 @@ fn main() {
     // }
     // assert_eq!(binary, 1);
 
+    // let mut v = vec![1, 2, 3, 4, 5];
+    // loop {
+    //     match v.pop() {
+    //         Some(x) => println!("{}", x),
+    //         None => break,
+    //     }
+    // }
+
     let mut v = vec![1, 2, 3, 4, 5];
-    loop {
-        match v.pop() {
-            Some(x) => println!("{}", x),
-            None => break,
-        }
+    while let Some(x) = v.pop() {
+        println!("{}", x);
     }
 }
 
