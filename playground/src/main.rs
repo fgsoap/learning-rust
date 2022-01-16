bc2f715be4738046550ee86b964e03ac907ceb3e
fgsoap <fgsoap@gmail.com>
2022-01-16 21:21:44 +0800

    add VecDeque

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::VecDeque;
 fn main() {
     // println!("Hello, world!");
     // let v = "hello world!";
@@ -271,6 +272,18 @@ fn main() {
     v3.push(5);
     v3.push(6);
     assert_eq!(v3, vec![4, 5, 6]);
+
+    let mut buf = VecDeque::new();
+    buf.push_front(1);
+    buf.push_front(2);
+    assert_eq!(buf.get(0), Some(&2));
+    assert_eq!(buf.get(1), Some(&1));
+    buf.push_back(3);
+    buf.push_back(4);
+    buf.push_back(5);
+    assert_eq!(buf.get(2), Some(&3));
+    assert_eq!(buf.get(3), Some(&4));
+    assert_eq!(buf.get(4), Some(&5));
 }
 
 // pub fn math(op: fn(i32, i32) -> i32, a: i32, b: i32) -> i32 {
